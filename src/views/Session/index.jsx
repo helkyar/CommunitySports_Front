44e4b442ab98898136c667451ff87b9e1ec9cc37
Pablo <pabloperezsada98@gmail.com>
2022-06-16 20:08:39 +0200

    fixed login/register modal

--- src/views/Session/index.jsx
+++ src/views/Session/index.jsx
@@ -2,21 +2,23 @@ import React, { useState } from "react";
 import { Login } from "components/Login";
 import { Register } from "components/Register";
 import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
 export const Session = () => {
-  const navigate = useNavigate();
+
   const [logView, setLogView] = useState(true);
   return (
     <>
       {logView && <Login />}
       {!logView && <Register />}
-      <button
-        className="session-btn"
-        onClick={() => setLogView((state) => !state)}
-      >
-        {logView ? "Go Register" : "Go Login"}
-      </button>
-      <button onClick={() => navigate("/")}>back</button>
+      <div className="session-btn-container">
+        <Button
+          className="session-btn"
+          onClick={() => setLogView((state) => !state)}
+        >
+          {logView ? "Go Register" : "Go Login"}
+        </Button>
+      </div>
     </>
   );
 };

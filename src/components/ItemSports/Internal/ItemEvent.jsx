0fb7aa38ecb1880ed59e84d80fbd5f48ea6318cf
Miguel Cid <miguel.cid.paz@gmail.com>
2022-06-16 20:02:10 +0200

    Estilos de botones para ir a eventos

--- src/components/ItemSports/Internal/ItemEvent.jsx
+++ src/components/ItemSports/Internal/ItemEvent.jsx
@@ -4,7 +4,7 @@ export const ItemEvent = ({ event }) => {
 
     return (
         <section className="item_sports--event-main">
-            <p className="item_sports--event-subtitle">{`Evento - ${event.name}`}<Link to={`/events/detail/${event.id}`}>GO</Link></p>
+            <p className="item_sports--event-subtitle">{`Evento - ${event.name}`}<button className="item_sports--button"><Link className="item_sports--Link" to={`/events/detail/${event.id}`}>GO</Link></button></p>
         </section>
     )
 }

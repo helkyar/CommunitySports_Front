9447839f1c8d9b501486f62663a01e15b6cf7d5d
Pablo <pabloperezsada98@gmail.com>
2022-06-16 21:03:25 +0200

    translation implemented

--- src/views/Session/index.jsx
+++ src/views/Session/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Login } from "components/Login";
 import { Register } from "components/Register";
-import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 export const Session = () => {
 
+  const [t, i18n] = useTranslation("global");
   const [logView, setLogView] = useState(true);
   return (
     <>
@@ -16,7 +17,7 @@ export const Session = () => {
           className="session-btn"
           onClick={() => setLogView((state) => !state)}
         >
-          {logView ? "Go Register" : "Go Login"}
+          {logView ? t('forms.go-register') : t('forms.go-login')}
         </Button>
       </div>
     </>
